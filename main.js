c6881434a0142054eb7443e6bedc74ac3649789b
Richard <Richardndjee@outlook.com>
2023-02-01 14:14:14 -0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function showStep2() {
+    document.getElementById("step1").style.display = "none";
+    document.getElementById("step2").style.display = "grid";
+  }
+  
+  function showStep1() {
+    document.getElementById("step1").style.display = "grid";
+    document.getElementById("step2").style.display = "none";
+  }
+  
